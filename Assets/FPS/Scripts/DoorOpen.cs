6d5eea7f9b9acd106b44c2f91938846f1a88f998
anh tiến <tienanh.do2510@gmail.com>
2022-03-17 16:11:28 +0700

    first commit

---
+++ Assets/FPS/Scripts/DoorOpen.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpen : MonoBehaviour
+{
+    public GameObject theDoor;
+    public AudioSource doorFX;
+    void OnTriggerEnter(Collider other)
+    {
+        doorFX.Play();
+        theDoor.GetComponent<Animator>().Play("DoorOpen");
+        StartCoroutine(closedoor());
+    }
+
+    IEnumerator closedoor()
+    {
+        yield return new WaitForSeconds(10);
+        doorFX.Play();
+        theDoor.GetComponent<Animator>().Play("DoorClose");
+    }
+}
